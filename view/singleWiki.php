fa80dcd5f846011892e19e4850fc05b9fdddd41d
ahmed <benseddiqahmed4@gmail.com>
2024-01-12 12:08:08 +0100

    commit

--- view/singleWiki.php
+++ view/singleWiki.php
@@ -1,3 +1,7 @@
+<?php
+require_once '../controller/displaywikisController.php';
+
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -38,28 +42,26 @@
         </nav>
         
         <section class="py-5">
-            <div class="container px-4 px-lg-5 my-5">
-                <div class="row gx-4 gx-lg-5 align-items-center">
-                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://dummyimage.com/600x700/dee2e6/6c757d.jpg" alt="..." /></div>
-                    <div class="col-md-6">
-                        <!-- <div class="small mb-1">SKU: BST-498</div> -->
-                        <h1 class="display-5 fw-bolder">wiki name</h1>
-                        <div class="fs-5 mb-5">
-                            <!-- <span class="text-decoration-line-through"> </span> -->
-                            <span>category</span>
-                        </div>
-                        <p class="lead">contenu</p>
-                        <div class="d-flex">
-                            <!-- <input class="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" /> -->
-                            <button class="btn btn-outline-dark flex-shrink-0" type="button">
-                                <i class="bi bi-tag me-1"></i>
-                                tag
-                            </button>
-                        </div>
+        <div class="container px-4 px-lg-5 my-5">
+            <div class="row gx-4 gx-lg-5 align-items-center">
+                <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://dummyimage.com/600x700/dee2e6/6c757d.jpg" alt="..." /></div>
+                <div class="col-md-6">
+                    <h1 class="display-5 fw-bolder"><?= $wiki['titre']; ?></h1>
+                    <div class="fs-5 mb-5">
+                        <span><?= $category ? $category['cat_name'] : 'Unknown Category'; ?></span>
+                    </div>
+                    <p class="lead"><?= $wiki['contenu']; ?></p>
+                    <div class="d-flex">
+                        <button class="btn btn-outline-dark flex-shrink-0" type="button">
+                            <i class="bi bi-tag me-1"></i>
+                            tag
+                        </button>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+    </section>
+    
 
         <footer class="py-5 bg-dark">
             <div class="container"><p class="m-0 text-center text-white">Copyright &copy; wickizone 2024</p></div>
